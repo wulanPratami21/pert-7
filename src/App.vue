fb5dec0081e60a3f5b3ac08d511cdb8e78bf56df
wulanPratami21 <72369595+wulanPratami21@users.noreply.github.com>
2021-04-26 17:39:45 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="nav">
+    <div class="container">
+    <navbar />
+    <router-view/>
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  }
+}
+</script>
